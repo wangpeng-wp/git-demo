6c9d500227f36a2d8fbc9927930cb991b0d6c698
wangpeng-wp <584771590@qq.com>
2023-10-22 11:54:06 +0800

    测试

---
+++ src/com/wp/test/git_test.java
@@ -0,0 +1,11 @@
+package com.wp.test;
+
+/**
+ * Created by wp on 2023/10/22.
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("测试！！");
+        System.out.println("测试！！");
+    }
+}
